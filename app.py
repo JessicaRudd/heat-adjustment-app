9d58413f41b1573e8c3bb575b0eaba5e26a77a80
JessicaRudd <jess@irudd.com>
2023-04-13 10:35:50 -0400

    changing back to port 5000

--- app.py
+++ app.py
@@ -74,5 +74,5 @@ if __name__ == '__main__':
     t.start()
 
     # Start Flask app
-    port = int(os.environ.get('PORT', 5001))
+    port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
